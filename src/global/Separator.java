ab69b472c29fe2f2960f24b39a7d4faa1115ef53
first <shg9094@naver.com>
2016-03-23 17:50:23 +0900

    u9i

---
+++ src/global/Separator.java
@@ -0,0 +1,15 @@
+package global;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Separator {
+	public static String[] extrack(HttpServletRequest request){
+		String[] arr = new String[2];
+		String path = request.getServletPath();
+		String temp = path.split("/")[2];
+		arr[0] = path.split("/")[1];
+		arr[1] = temp.substring(0, temp.indexOf("."));
+		
+		return arr;
+	}
+}
